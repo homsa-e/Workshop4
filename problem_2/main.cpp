c0b2ed09af45967aea9d935744d5ebd0559d4458
homsa-e <55227510+homsa-e@users.noreply.github.com>
2019-11-13 12:40:38 +0300

    Update main.cpp

--- problem_2/main.cpp
+++ problem_2/main.cpp
@@ -1,9 +1,23 @@
-/// Recreate the multiplication table task, allowing the user to rerun the program again after it prints the multiplication table.
-
 #include <iostream>
 
 int main()
 {
-    /// input your code here
+    int a, b, i, j;
+    std::string ans;
+    do {
+        std::cin >> a >> b;
+        for (i = 1; i <= a; i++){
+            std::cout << "\t" << i;
+        }
+        std::cout << std::endl;
+        for (j = 1; j <= a; j++) {
+            std::cout << j;
+            for (i = 1; i <= a; i++)
+                std::cout << "\t" << (j * i) % b;
+            std::cout << std::endl;
+        }
+        std::cout << "Do you want to rerun the program? Press 'Y' + Enter if yes, otherwise press any other key + Enter" << std::endl;
+        std::cin >> ans;
+    } while (ans == "y" || ans == "Y");
     return 0;
 }
